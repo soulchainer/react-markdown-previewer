752887b29f733a7de253519359a82d8e3f8a7bed
soulchainer <soulchainer@gmail.com>
2016-12-22 19:05:24 +0100

    Serve the project with GitHub Pages

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const autoprefixer = require('autoprefixer'); // eslint-disable-line import/no-extraneous-dependencies
-
-module.exports = {
-  plugins: [
-    autoprefixer({ browsers: ['last 2 versions'] }),
-  ],
-};
--- webpack.config.babel.js
+++
@@ -1,7 +0,0 @@
-import Config, { environment } from 'webpack-config'; // eslint-disable-line import/no-extraneous-dependencies
-
-environment.setAll({
-  env: () => process.env.NODE_ENV,
-});
-
-export default new Config().extend('config/webpack.[env].config.babel.js');
